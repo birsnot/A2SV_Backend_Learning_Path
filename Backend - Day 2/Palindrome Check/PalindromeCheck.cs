65c237d0e51c5a7d3ef28e3db33e7da8e6b9fd22
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-08-02 01:36:10 -0300

    Add files via upload

---
+++ Backend - Day 2/Palindrome Check/PalindromeCheck.cs
@@ -0,0 +1,34 @@
+﻿public class PalindromeCheck
+{
+    public static void Main()
+    {
+        // test the function
+        Console.WriteLine("Enter a string to check if it's palindrome: ");
+        string test = Console.ReadLine();
+
+        if(isPalindrome(test))
+            Console.WriteLine($"{test} is palindrome");
+        else 
+            Console.WriteLine($"{test} is not palindrome");
+    }
+
+    static bool isPalindrome(string str)
+    {
+        string cleanedStr = "";
+
+        foreach (char ch in str)
+        {
+            if (char.IsLetterOrDigit(ch)) cleanedStr += char.ToLower(ch);
+        }
+
+        int len = cleanedStr.Length;
+        for(int i = 0; i < len/2; i++)
+        {
+            if (cleanedStr[i] != cleanedStr[len - 1 - i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Backend - Day 2/Word Frequency Count/WordFrequencyCount.cs
@@ -0,0 +1,44 @@
+﻿public class WordFrequencyCount
+{
+    public static void Main()
+    {
+        // test the function
+        Console.WriteLine("Enter a string to count its words: ");
+        string test = Console.ReadLine();
+        Dictionary<string, int> testCount = wordCounter(test);
+
+        // display the test output
+        Console.WriteLine();
+        Console.WriteLine($"{"WORD",-20}COUNT");
+        foreach(KeyValuePair<string, int> kvp in testCount)
+        {
+            Console.WriteLine($" {kvp.Key, -20}{kvp.Value}");
+        }
+    }
+
+    static Dictionary<string, int> wordCounter(string str)
+    {
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+        string word = "";
+
+        foreach(char ch in str)
+        {
+            if (char.IsLetterOrDigit(ch)) word += char.ToLower(ch);
+            else if(word.Length > 0)
+            {
+                if (wordCount.ContainsKey(word)) wordCount[word]++;
+                else wordCount[word] = 1;
+                word = "";
+            }
+        }
+
+        // this is to handle the last word
+        if (word.Length > 0)
+        {
+            if (wordCount.ContainsKey(word)) wordCount[word]++;
+            else wordCount[word] = 1;
+        }
+
+        return wordCount;
+    }
+}

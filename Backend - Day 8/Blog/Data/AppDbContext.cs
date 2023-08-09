77a180bddd1fbfad11b20baecc56ed113d25d158
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-08-09 13:29:02 -0300

    Add files via upload

---
+++ Backend - Day 8/Blog/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data;
+
+public class AppDbContext : DbContext
+{
+    public virtual DbSet<Post> Posts { get; set; }
+    public virtual DbSet<Comment> Comments { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Comment>(entity =>
+        {
+            entity.HasOne(p => p.Post)
+                .WithMany(c => c.Comments)
+                .HasForeignKey(x => x.PostId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_Comment_Post");
+        });
+    }
+}
---
+++ Backend - Day 8/Blog/Models/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Models;
+
+public class Comment : BaseEntity
+{
+    public int PostId { get; set; }
+    public string Text { get; set; } = "";
+    
+    public virtual Post Post { get; set; }
+}

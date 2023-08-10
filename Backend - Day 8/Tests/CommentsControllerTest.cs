9b9bdfb21737c4d86ae24356a445bbe361f7fe66
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-08-09 21:19:57 -0300

    Add files via upload

---
+++ Backend - Day 8/Tests/CommentsControllerTest.cs
@@ -0,0 +1,221 @@
+﻿using Blog.Controllers;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests;
+
+public class CommentsControllerTests
+    {
+        private DbContextOptions<AppDbContext> CreateNewContextOptions()
+        {
+            return new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [Fact]
+        public async Task Get_ReturnsOkResultWithComments()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Comments.Add(new Comment { Id = 1, Text = "Comment 1" });
+                context.Comments.Add(new Comment { Id = 2, Text = "Comment 2" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new CommentsController(context);
+
+                var result = await controller.Get();
+
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                var comments = Assert.IsAssignableFrom<List<Comment>>(okResult.Value);
+                Assert.Equal(2, comments.Count);
+            }
+        }
+
+        [Fact]
+        public async Task Get_WithValidCommentId_ReturnsOkResultWithComment()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Comments.Add(new Comment { Id = 1, Text = "Comment 1" });
+                context.Comments.Add(new Comment { Id = 2, Text = "Comment 2" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new CommentsController(context);
+
+                var result = await controller.Get(2);
+
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                var comment = Assert.IsAssignableFrom<Comment>(okResult.Value);
+                Assert.Equal("Comment 2", comment.Text);
+            }
+        }
+
+        [Fact]
+        public async Task Get_WithInvalidCommentId_ReturnsBadRequest()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Comments.Add(new Comment { Id = 1, Text = "Comment 1" });
+                context.Comments.Add(new Comment { Id = 2, Text = "Comment 2" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new CommentsController(context);
+
+                var result = await controller.Get(3);
+
+                var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+                Assert.Equal("Invalid Id", badRequestResult.Value);
+            }
+        }
+
+        [Fact]
+        public async Task Post_AddsCommentAndReturnsCreatedAtAction()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new CommentsController(context);
+                var newComment = new Comment { Id = 1, Text = "New Comment" };
+
+                var result = await controller.Post(newComment);
+
+                var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+                var comment = Assert.IsAssignableFrom<Comment>(createdAtActionResult.Value);
+                Assert.Equal("New Comment", comment.Text);
+
+                using (var dbContext = new AppDbContext(options))
+                {
+                    var savedComment = await dbContext.Comments.FindAsync(1);
+                    Assert.Equal("New Comment", savedComment.Text);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Put_WithValidCommentId_UpdatesCommentAndReturnsNoContent()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Comments.Add(new Comment { Id = 1, Text = "Comment 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new CommentsController(context);
+                var updatedComment = new Comment { Id = 1, Text = "Updated Comment" };
+
+                var result = await controller.Put(1, updatedComment);
+
+                Assert.IsType<NoContentResult>(result);
+
+                using (var dbContext = new AppDbContext(options))
+                {
+                    var comment = await dbContext.Comments.FindAsync(1);
+                    Assert.Equal("Updated Comment", comment.Text);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Put_WithInvalidCommentId_ReturnsBadRequest()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Comments.Add(new Comment { Id = 1, Text = "Comment 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new CommentsController(context);
+                var updatedComment = new Comment { Id = 2, Text = "Updated Comment" };
+
+                var result = await controller.Put(2, updatedComment);
+
+                var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+                Assert.Equal("Invalid id", badRequestResult.Value);
+
+                using (var dbContext = new AppDbContext(options))
+                {
+                    var comment = await dbContext.Comments.FindAsync(1);
+                    Assert.Equal("Comment 1", comment.Text);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Delete_WithValidCommentId_RemovesCommentAndReturnsNoContent()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Comments.Add(new Comment { Id = 1, Text = "Comment 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new CommentsController(context);
+
+                var result = await controller.Delete(1);
+
+                Assert.IsType<NoContentResult>(result);
+
+                using (var dbContext = new AppDbContext(options))
+                {
+                    var comment = await dbContext.Comments.FindAsync(1);
+                    Assert.Null(comment);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Delete_WithInvalidCommentId_ReturnsBadRequest()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Comments.Add(new Comment { Id = 1, Text = "Comment 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new CommentsController(context);
+
+                var result = await controller.Delete(2);
+
+                var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+                Assert.Equal("Invalid id", badRequestResult.Value);
+
+                using (var dbContext = new AppDbContext(options))
+                {
+                    var comment = await dbContext.Comments.FindAsync(1);
+                    Assert.Equal("Comment 1", comment.Text);
+                }
+            }
+        }
+    }
---
+++ Backend - Day 8/Tests/PostsControllerTest.cs
@@ -0,0 +1,257 @@
+﻿using Blog.Controllers;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests;
+
+public class PostsControllerTests
+    {
+        private DbContextOptions<AppDbContext> CreateNewContextOptions()
+        {
+            return new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [Fact]
+        public async Task Get_ReturnsOkResultWithPosts()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.Posts.Add(new Post { Id = 2, Title = "Post 2", Content = "Content 2" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+
+                var result = await controller.Get();
+
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                var posts = Assert.IsAssignableFrom<List<Post>>(okResult.Value);
+                Assert.Equal(2, posts.Count);
+            }
+        }
+
+        [Fact]
+        public async Task Get_WithValidPostId_ReturnsOkResultWithPost()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.Posts.Add(new Post { Id = 2, Title = "Post 2", Content = "Content 2" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+
+                var result = await controller.Get(2);
+
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                var post = Assert.IsAssignableFrom<Post>(okResult.Value);
+                Assert.Equal("Post 2", post.Title);
+            }
+        }
+
+        [Fact]
+        public async Task Get_WithInvalidPostId_ReturnsBadRequest()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.Posts.Add(new Post { Id = 2, Title = "Post 2", Content = "Content 2" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+
+                var result = await controller.Get(3);
+
+                var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+                Assert.Equal("Invalid Id", badRequestResult.Value);
+            }
+        }
+
+        [Fact]
+        public async Task Post_AddsPostAndReturnsCreatedAtAction()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+                var newPost = new Post { Id = 1, Title = "New Post", Content = "New Content" };
+
+                var result = await controller.Post(newPost);
+
+                var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+                var post = Assert.IsAssignableFrom<Post>(createdAtActionResult.Value);
+                Assert.Equal("New Post", post.Title);
+
+                using (var dbContext = new AppDbContext(options))
+                {
+                    var savedPost = await dbContext.Posts.FindAsync(1);
+                    Assert.Equal("New Post", savedPost.Title);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Patch_WithValidPostId_UpdatesPostAndReturnsNoContent()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+
+                var result = await controller.Patch(1, "Updated Title");
+
+                Assert.IsType<NoContentResult>(result);
+
+                using (var dbContext =new AppDbContext(options))
+                {
+                    var updatedPost = await dbContext.Posts.FindAsync(1);
+                    Assert.Equal("Updated Title", updatedPost.Title);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Patch_WithInvalidPostId_ReturnsBadRequest()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+
+                var result = await controller.Patch(2, "Updated Title");
+
+                var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+                Assert.Equal("Invalid id", badRequestResult.Value);
+            }
+        }
+
+        [Fact]
+        public async Task Put_WithValidPostId_UpdatesPostAndReturnsNoContent()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+                var updatedPost = new Post { Title = "Updated Title", Content = "Updated Content" };
+
+                var result = await controller.Put(1, updatedPost);
+
+                Assert.IsType<NoContentResult>(result);
+
+                using (var dbContext = new AppDbContext(options))
+                {
+                    var post = await dbContext.Posts.FindAsync(1);
+                    Assert.Equal("Updated Title", post.Title);
+                    Assert.Equal("Updated Content", post.Content);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Put_WithInvalidPostId_ReturnsBadRequest()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+                var updatedPost = new Post { Title = "Updated Title", Content = "Updated Content" };
+
+                var result = await controller.Put(2, updatedPost);
+
+                var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+                Assert.Equal("Invalid id", badRequestResult.Value);
+            }
+        }
+
+        [Fact]
+        public async Task Delete_WithValidPostId_RemovesPostAndReturnsNoContent()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+
+                var result = await controller.Delete(1);
+
+                Assert.IsType<NoContentResult>(result);
+
+                using (var dbContext = new AppDbContext(options))
+                {
+                    var post = await dbContext.Posts.FindAsync(1);
+                    Assert.Null(post);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Delete_WithInvalidPostId_ReturnsBadRequest()
+        {
+            var options = CreateNewContextOptions();
+            using (var context = new AppDbContext(options))
+            {
+                context.Posts.Add(new Post { Id = 1, Title = "Post 1", Content = "Content 1" });
+                context.SaveChanges();
+            }
+
+            using (var context = new AppDbContext(options))
+            {
+                var controller = new PostsController(context);
+
+                var result = await controller.Delete(2);
+
+                var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+                Assert.Equal("Invalid id", badRequestResult.Value);
+            }
+        }
+    }
